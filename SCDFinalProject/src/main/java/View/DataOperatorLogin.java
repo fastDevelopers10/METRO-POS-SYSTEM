534a332f6c89fd3b37818ab5d3c0da9e742511cd
Aleena <alinarafiq0676@gmail.com>
2024-12-01 20:55:58 +0500

    DataOperator UI

--- SCDFinalProject/src/main/java/View/DataOperatorLogin.java
+++ SCDFinalProject/src/main/java/View/DataOperatorLogin.java
@@ -14,8 +14,10 @@ public class DataOperatorLogin extends JFrame {
     public DataOperatorLogin() {
         setTitle("Data Operator Login");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(1920, 1080); // Set a default size for the window
-        setResizable(false);
+        // Get screen size and set JFrame to this
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize(screenSize);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
         setLocationRelativeTo(null); // Center the window on screen
 
         // Load the background image
@@ -45,14 +47,14 @@ public class DataOperatorLogin extends JFrame {
         JTextField tfUsername = new JTextField();
         tfUsername.setOpaque(true); // Make background visible (transparent is false)
         tfUsername.setBorder(new LineBorder(Color.BLACK, 1)); // Add a black border
-        tfUsername.setBounds(490, 260, 350, 30); // Position and size (x, y, width, height)
+        tfUsername.setBounds(490, 240, 350, 30); // Position and size (x, y, width, height)
         panel.add(tfUsername); // Add to the panel
 
         // Create the second text field (password)
         JTextField tfPassword = new JTextField();
         tfPassword.setOpaque(true); // Make background visible (transparent is false)
         tfPassword.setBorder(new LineBorder(Color.BLACK, 1)); // Add a black border
-        tfPassword.setBounds(490, 345, 350, 30); // Position and size (x, y, width, height)
+        tfPassword.setBounds(490, 310, 350, 30); // Position and size (x, y, width, height)
         panel.add(tfPassword); // Add to the panel
 
         // Create the "Login" button
@@ -62,7 +64,7 @@ public class DataOperatorLogin extends JFrame {
         btnLogin.setBorderPainted(false); // Make the border invisible
         btnLogin.setForeground(Color.WHITE); // Set text color to yellow
         btnLogin.setFont(new Font("Century Gothic", Font.PLAIN, 16)); // Optional: Set custom font
-        btnLogin.setBounds(540, 416, 120, 40); // Position and size (x, y, width, height)
+        btnLogin.setBounds(527, 354, 120, 40); // Position and size (x, y, width, height)
         panel.add(btnLogin); // Add to the panel
 
         // Create the "Exit" button
@@ -72,7 +74,7 @@ public class DataOperatorLogin extends JFrame {
         btnExit.setBorderPainted(false); // Make the border invisible
         btnExit.setForeground(Color.BLACK); // Set text color to yellow
         btnExit.setFont(new Font("Century Gothic", Font.PLAIN, 16)); // Optional: Set custom font
-        btnExit.setBounds(660, 416, 120, 40); // Position and size (x, y, width, height)
+        btnExit.setBounds(639, 354, 120, 40); // Position and size (x, y, width, height)
         panel.add(btnExit); // Add to the panel
 
         // Add the custom panel to the frame

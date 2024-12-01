dce05a041cde99b6de1acb5e2fa19546db0d5d68
Aleena <alinarafiq0676@gmail.com>
2024-12-01 19:35:38 +0500

    DataOperator UI

--- SCDFinalProject/src/main/java/View/DataOperatorLogin.java
+++ SCDFinalProject/src/main/java/View/DataOperatorLogin.java
@@ -5,7 +5,6 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
